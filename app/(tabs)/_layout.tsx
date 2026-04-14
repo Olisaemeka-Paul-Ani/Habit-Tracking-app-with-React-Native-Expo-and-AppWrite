90afe67b8db0649d8a1329be827e462b957b2ce3
Olisaemeka Paul Ani <oliandchildren@gmail.com>
2026-04-13 22:26:34 -0700

    Feat: implement Appwrite config and auth UI components

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,24 @@
 import AntDesign from '@expo/vector-icons/AntDesign';
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
-import { Tabs } from "expo-router";
+import { Redirect, Tabs } from "expo-router";
 
 export default function Layout() {
+  const isAuth = false;
+
+  if (!isAuth) {
+    return <Redirect href="/auth" />;
+  }
+
   return (
     <Tabs screenOptions={{ tabBarActiveTintColor: "coral" }}>
-      <Tabs.Screen name="index" options={{ title: "Home", tabBarIcon: ({ color, focused }) => focused ? <FontAwesome5 name="home" size={24} color= {color} /> : <AntDesign name="home" size={24} color={color} /> }} />
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, focused }) =>
+            focused ? <FontAwesome5 name="home" size={24} color={color} /> : <AntDesign name="home" size={24} color={color} />,
+        }}
+      />
       <Tabs.Screen name="login" options={{ title: "Login" }} />
     </Tabs>
   );
---
+++ app/auth.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import { KeyboardAvoidingView, Platform, View } from "react-native";
+import { Button, Text, TextInput } from "react-native-paper";
+
+export default function AuthScreen (){
+  const [isSignUp, setIsSignUp] = useState<boolean>(false)
+  return (
+  <KeyboardAvoidingView behavior = {Platform.OS === "ios"? "padding": "height"}>
+    <View>
+      <Text>
+        {isSignUp? "Create Account" : "Welcome Back"}
+      </Text>
+      <TextInput label = "Email" autoCapitalize="none" keyboardType = "email-address" placeholder = "example@gmail.com" mode = "outlined" />
+      <TextInput label = "Password" autoCapitalize="none" keyboardType = "email-address" mode = "outlined" />
+      <Button mode = "contained">Sign Up</Button>
+      <Button mode = "text">Already have an account? Sign In</Button>
+    </View>
+  </KeyboardAvoidingView>);
+}
