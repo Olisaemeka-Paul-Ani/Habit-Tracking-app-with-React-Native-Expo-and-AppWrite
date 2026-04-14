0cca669d03b680276083757e91e0b3a4bfbb1c8e
Olisaemeka Paul Ani <oliandchildren@gmail.com>
2026-04-14 00:54:22 -0700

    Feat: implement auth navigation logic and UI legibility enhancements

--- app/auth.tsx
+++ app/auth.tsx
@@ -1,19 +1,54 @@
 import { useState } from "react";
-import { KeyboardAvoidingView, Platform, View } from "react-native";
+import { KeyboardAvoidingView, Platform, StyleSheet, View } from "react-native";
 import { Button, Text, TextInput } from "react-native-paper";
 
 export default function AuthScreen (){
   const [isSignUp, setIsSignUp] = useState<boolean>(false)
+   const handleSwitchMode = () => {
+    setIsSignUp((prev) => !prev)
+  };
   return (
-  <KeyboardAvoidingView behavior = {Platform.OS === "ios"? "padding": "height"}>
-    <View>
-      <Text>
+  <KeyboardAvoidingView behavior = {Platform.OS === "ios"? "padding": "height"} style = {styles.container}>
+    <View style = {styles.content}>
+      <Text style = {styles.title} variant="headlineMedium">
         {isSignUp? "Create Account" : "Welcome Back"}
       </Text>
-      <TextInput label = "Email" autoCapitalize="none" keyboardType = "email-address" placeholder = "example@gmail.com" mode = "outlined" />
-      <TextInput label = "Password" autoCapitalize="none" keyboardType = "email-address" mode = "outlined" />
-      <Button mode = "contained">Sign Up</Button>
-      <Button mode = "text">Already have an account? Sign In</Button>
+      <TextInput style = {styles.input} label = "Email" autoCapitalize="none" keyboardType = "email-address" placeholder = "example@gmail.com" mode = "outlined" />
+      <TextInput style = {styles.input} label = "Password" autoCapitalize="none" keyboardType = "email-address" mode = "outlined" />
+      <Button mode = "contained">{isSignUp? "Sign Up": "Sign In"}</Button>
+      <Button style = {styles.button} mode = "text" onPress={handleSwitchMode}>{isSignUp? "Already have an account? Sign In": "Don't have an account, Sign Up"}</Button>
     </View>
   </KeyboardAvoidingView>);
+
+
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: "#f5f5f5"
+  },
+
+  content: {
+    flex: 1,
+    padding: 16,
+    justifyContent: "center",
+  }, 
+
+  title: {
+    textAlign: "center",
+    marginBottom: 24,
+  }, 
+
+  input: {
+    marginBottom: 16,
+  }, 
+
+  button: {
+    marginTop: 8,
+  }, 
+
+  switchModeButton: {
+    marginTop: 16,
+  }, 
+})
